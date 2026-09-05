d83fc78163b2c07c979a57c4e08cb8a5355fd801
Sakshi13-05 <sakshichavan61116@gmail.com>
2026-09-05 11:49:46 +0530

    Clean up unused folder structure

--- Frontend/src/services/authService.js
+++
@@ -1,9 +0,0 @@
-import api from "./api";
-
-export const signup = (userData) => {
-    return api.post("/auth/signup", userData);
-};
-
-export const login = (userData) => {
-    return api.post("/auth/login", userData);
-};
